944e83c205fb4b3ede5e46c25af313d630f25caa
Felipe Leres <leresfelipe@gmail.com>
2025-10-07 21:38:49 -0300

    iniciando projeto, criando primeiras classes e seus relacionamentos

---
+++ flcommerce/src/main/java/com/felipeleres/flcommerce/FlcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.felipeleres.flcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlcommerceApplication.class, args);
+	}
+
+}
