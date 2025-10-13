848d427b84d79e371c6950bd392a1f9268ce66e9
Felipe Leres <leresfelipe@gmail.com>
2025-10-12 22:08:49 -0300

    GET products paged

--- flcommerce/src/main/java/com/felipeleres/flcommerce/controllers/ProductController.java
+++ flcommerce/src/main/java/com/felipeleres/flcommerce/controllers/ProductController.java
@@ -5,11 +5,14 @@ import com.felipeleres.flcommerce.dto.ProductDTO;
 
 import com.felipeleres.flcommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,6 +26,11 @@ public class ProductController {
     public ProductDTO findById (@PathVariable Long id) {
         ProductDTO productDTO = productService.findByID(id);
         return productDTO;
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll (Pageable pageable) {
+        return productService.findAll(pageable);
     }
 
 }
